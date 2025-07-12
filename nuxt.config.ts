53f861d98212bf1b73725067e20805a01c0948f1
alfat osk <riddbengkok@gmail.com>
2025-07-12 14:51:27 +0700

    setup build deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  ssr: true,
+  app: {
+    head: {
+      title: 'Nuxt 3',
+      meta: [
+        { name: 'description', content: 'Nuxt 3' }
+      ]
+    },
+    baseURL: '/',
+  }
 })
